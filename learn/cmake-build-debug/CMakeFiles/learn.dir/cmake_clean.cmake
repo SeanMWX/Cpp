34b9a2136f52eac6c1b12dbde926ad496ccb142d
Xinhai Zou <xinhai.zou@hotmail.com>
2022-04-22 00:36:44 +0200

    ch 2.5.2.

---
+++ learn/cmake-build-debug/CMakeFiles/learn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn.dir/main.cpp.obj"
+  "CMakeFiles/learn.dir/main.cpp.obj.d"
+  "learn.exe"
+  "learn.exe.manifest"
+  "learn.pdb"
+  "liblearn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- learn/main.cpp
+++ learn/main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
 
+bool accept() {
+    std::cout << "Do you want to proceed (y or n)?\n";
+    char answer = 0;
+    std::cin >> answer;
+
+    switch (answer) {
+        case 'y':
+            return true;
+        case 'n':
+            return false;
+        default:
+            std::cout << "I'll take that for n.";
+            return false;
+    }
+}
+
+namespace Stack {
+    const int MAX_SIZE = 1;
+    char v[MAX_SIZE];
+    int top = 0;
+    class Overflow {};
+    class Underflow{};
+
+    void push(char);
+    char pop();
+
+    void push(char c) {
+        try {
+            if (top == MAX_SIZE) throw Overflow();
+            else{
+                v[top] = c;
+                top++;
+            }
+        } catch (Stack::Overflow) {
+            std::cout << "!!Stack is Full!! Will not do anything." << std::endl;
+        }
+    }
+
+    char pop() {
+        try {
+            if (top == 0) throw Underflow();
+            else {
+                return v[--top];
+            }
+        } catch (Stack::Underflow) {
+            std::cout << "!!Stack is Empty!! Will not do anything." << std::endl;
+        }
+        return NULL;
+    }
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    Stack::push('b');
+    Stack::push('b');
+    std::cout << Stack::pop() << std::endl;
+    Stack::pop();
+    Stack::pop();
+    std::cout << "End." << std::endl;
+
+    // 2.5.2.
+
     return 0;
 }
